c4880b37ac7622b0737593ee917594e2794a126f
Jan Schill <janschill@me.com>
2020-02-24 23:45:12 +0100

    Use seeder to populate database

---
+++ app/db/seeder.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'bcrypt'
+require 'faker'
+require 'sequel'
+require 'literate_randomizer'
+
+DB = Sequel.connect('sqlite://db/development.db')
+DB[:users].truncate
+DB[:messages].truncate
+DB[:followers].truncate
+users = DB[:users]
+messages = DB[:messages]
+followers = DB[:followers]
+
+10.times do
+  users.insert(
+    email: Faker::Internet.email,
+    username: "#{Faker::Name.first_name}_#{Time.now.to_i}",
+    password: BCrypt::Password.create('secret')
+  )
+end
+
+10.times do
+  followers.insert(
+    who_id: users.all.sample[:user_id],
+    whom_id: users.all.sample[:user_id]
+  )
+end
+
+100.times do
+  messages.insert(
+    user_id: users.all.sample[:user_id],
+    text: LiterateRandomizer.sentence,
+    pub_date: Time.now.to_i,
+    flagged: 0
+  )
+end
