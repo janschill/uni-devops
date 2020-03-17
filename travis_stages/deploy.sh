5ee0f2720603476f95cb5eb9281e56b2f7c39598
Alexandre Vanini <vanini.alexandre@gmail.com>
2020-03-17 21:17:41 +0100

    add /root/ to deploy stage

--- travis_stages/deploy.sh
+++ travis_stages/deploy.sh
@@ -8,7 +8,7 @@ eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
-ssh -i ./deploy_key -o "StrictHostKeyChecking no" root@janschill.de "docker-compose -f /minitwit/docker-compose.yml down && docker-compose -f /root/minitwit/docker-compose.yml up -d"
+ssh -i ./deploy_key -o "StrictHostKeyChecking no" root@janschill.de "docker-compose -f /root/minitwit/docker-compose.yml down && docker-compose -f /root/minitwit/docker-compose.yml up -d"
 
 # Keeping this line here for debug purpose
 # ssh -i ./deploy_key -o "StrictHostKeyChecking no" root@janschill.de "docker rm -f minitwit && docker pull freakency/devops && docker run --name minitwit -v /root/db:/var/www/db -p 80:80 -p 1337:1337 --rm -d freakency/devops"
