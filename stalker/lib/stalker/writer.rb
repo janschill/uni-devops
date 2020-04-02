659a0452846af5bcc907739aa7d66c9c16331543
Jan Schill <janschill@me.com>
2020-04-02 12:21:33 +0200

    Rescue exception of unreachable server

--- stalker/lib/stalker/writer.rb
+++ stalker/lib/stalker/writer.rb
@@ -16,10 +16,14 @@ module Stalker
       File.new(@filename, 'w') unless File.exist?(@filename)
     end
 
-    def write(connector, site)
+    def write(message)
+      File.write(@filename, "#{message}\n", mode: 'a')
+    end
+
+    def write_connection(connector, site)
       now = Time.now.strftime('%Y-%m-%dT%H:%M:%S.%L%z') # ISO 8061
       content = "[#{now}] \"GET #{site['path']}\" #{connector.status} #{Formatter.seconds_to_milli(connector.response_time)}"
-      File.write(@filename, "#{content}\n", mode: 'a')
+      write(content)
     end
   end
 end
