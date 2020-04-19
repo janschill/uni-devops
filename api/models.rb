225152ac3b57f043d9794e0efb4395063d5dbc51
Jan Schill <janschill@me.com>
2020-04-19 14:12:02 +0200

    Fix Rubocop

--- api/models.rb
+++ api/models.rb
@@ -7,13 +7,15 @@ require 'yaml'
 module MiniTwit
   databases = YAML.load_file('config/database.yml')
 
-  DB = Sequel.mysql2("#{databases['production']['database']}",
-    user:'doadmin', 
-    password: "#{databases['default']['password']}",
+  DB = Sequel.mysql2(
+    databases[AppEnvironment.environment.to_s]['database'].to_s,
+    user: 'doadmin',
+    password: databases['default']['password'].to_s,
     host: 'minitwit-db-do-user-3981230-0.a.db.ondigitalocean.com',
-    port: 25060, 
+    port: 25060,
     max_connections: 10,
-    sslmode: 'require')
+    sslmode: 'require'
+  )
 
   Model = Class.new(Sequel::Model)
   Model.db = DB
