0b5251bef8b89a07d97814b4645f7daa442d55fc
Jan Schill <janschill@me.com>
2020-04-19 14:56:24 +0200

    Add more analytics to notebook (#175)
    * Switch database to mysql for App
    
    * Switch database to mysql for Api
    
    * Fix wrong API gems. Sort gems because rubocop
    
    * Fix Rubocop
    
    * Add more analytics
    
    Co-authored-by: Frederik Schou Schmidt <frederik@schouschmidt.dk>
    Co-authored-by: Alexandre Vanini <vanini.alexandre@gmail.com>

--- app/models.rb
+++ app/models.rb
@@ -6,9 +6,15 @@ require 'yaml'
 # Database and ORM layers
 module MiniTwit
   databases = YAML.load_file('config/database.yml')
-  DB = Sequel.connect(
-    "#{databases['default']['adapter']}://"\
-    "#{databases[AppEnvironment.environment.to_s]['database']}"
+
+  DB = Sequel.mysql2(
+    databases[AppEnvironment.environment.to_s]['database'].to_s,
+    user: 'doadmin',
+    password: databases['default']['password'].to_s,
+    host: 'minitwit-db-do-user-3981230-0.a.db.ondigitalocean.com',
+    port: 25060,
+    max_connections: 10,
+    sslmode: 'require'
   )
 
   Model = Class.new(Sequel::Model)
