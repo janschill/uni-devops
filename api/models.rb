b18feab8bc130456d840f7a7433785b2a89cdc62
Frederik Schou Schmidt <frederik@schouschmidt.dk>
2020-04-22 17:02:28 +0200

    Rubocop style fix

--- api/models.rb
+++ api/models.rb
@@ -8,7 +8,7 @@ module MiniTwit
   databases = YAML.load_file('config/database.yml')
 
   DB = Sequel.mysql2(
-    "minitwit_test",
+    'minitwit_test',
     user: 'doadmin',
     password: databases['default']['password'].to_s,
     host: 'minitwit-db-do-user-3981230-0.a.db.ondigitalocean.com',
